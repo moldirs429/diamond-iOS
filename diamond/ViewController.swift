e9c44638895819afd6cf551262f069c64a1da477
Moldir Shynggys <75599135+moldirs429@users.noreply.github.com>
2023-04-17 16:15:51 +0200

    Add files via upload

---
+++ diamond/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  diamond
+//
+//  Created by Moldir Shynggys on 09.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
